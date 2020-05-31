40bee9370e4d640eb142e414dbead22b72fb439d
lesuyo0621 <lesuyo930621@naver.com>
2020-06-01 02:22:42 +0900

    Todojs web until weather api site signup

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "6e07a4c17c94e01a19554dd77847f700";
+const COORDS = "coords";
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("Error");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }else{
+
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
